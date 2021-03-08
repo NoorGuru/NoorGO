de5216627df189ef46de9bd9b48f34a1e1eb8c11
mohnoor94 <moh.noor94@gmail.com>
2021-03-08 23:24:50 +0200

    Initial work

---
+++ redirect.js
@@ -0,0 +1,15 @@
+const redirections = {
+    "akf": "https://www.abukhleif.com/",
+    "apps": "https://play.google.com/store/apps/dev?id=6213463466804826025",
+    "cv": "https://drive.google.com/file/d/0B2215F1Cj2_0ZF9FVFlpNXhSUmM/view",
+};
+
+const getLongUrl = (shortUrl) => redirections[shortUrl] || "https://www.noor.guru/";
+
+const redirect = () => {
+    const currentPage = window.location.href;
+    const shortUrl = currentPage?.split("/").pop() || "";
+    window.location.replace(getLongUrl(shortUrl));
+}
+
+redirect();
