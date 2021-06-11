4fdb42e152ac2e92354b89c311969f5618db27b6
mohnoor94 <moh.noor94@gmail.com>
2021-06-11 20:10:38 +0300

    Add C++ links

--- redirect.js
+++ redirect.js
@@ -11,6 +11,19 @@ const redirections = {
     "youtube-main": "https://www.youtube.com/c/MohammadNoorAbuKhleif",
     "ig": "https://www.instagram.com/mohnoor94/",
     "sof": "https://stackoverflow.com/users/2847543/mohammad-noor",
+
+    // C++ Files
+    "cpp-fts": "https://1drv.ms/b/s!AnR5_MGIlB3e72UGY6djrr2v5uXy",
+    "cpp-ftq1": "https://1drv.ms/b/s!AnR5_MGIlB3e72ki6llgN-pEmQ5Z",
+    "cpp-ftq2": "https://www.abukhleif.com/first-exam-important-questions/",
+    "cpp-sds1": "https://1drv.ms/b/s!AnR5_MGIlB3e72Soidf48IJz93eA",
+    "cpp-sds2": "https://1drv.ms/b/s!AnR5_MGIlB3e72Soidf48IJz93eA",
+    "cpp-sdq1": "https://1drv.ms/b/s!AnR5_MGIlB3e70lVF0ECLRIpd8tm",
+    "cpp-sdq2": "https://www.abukhleif.com/cpp-second-exam-write-function-past-papers/",
+    "cpp-sdq3": "https://www.abukhleif.com/pattern-programs/",
+    "cpp-fls": "https://1drv.ms/b/s!AnR5_MGIlB3e70iHhYr8aGXhtPuC",
+    "cpp-flq": "https://1drv.ms/b/s!AnR5_MGIlB3e70W4gP1sjOm4iea2",
+    "cpp-ex": "https://www.abukhleif.com/category/c/cppexercises/",
 };
 
 const getLongUrl = (shortUrl) => redirections[shortUrl] || "https://www.noor.guru/";
