2571f4edc22ad54df93b503ecd9ff91aa7a3adb5
mohnoor94 <moh.noor94@gmail.com>
2026-06-06 21:06:10 +0300

    chore(redirect): update presentation domain and disable C++ redirects
    - Update presentation URLs from noor.guru to bynoor.io domain
    - Comment out all C++ related redirects (cpp-fts, cpp-ftq*, cpp-sds*, cpp-sdq*, cpp-fls, cpp-flq, cpp-ex)
    - Comment out all C++ quiz redirects (cpp-qft*, cpp-eft*)
    - Consolidates domain migration and archived content management

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 const redirections = {
-    "akf": "https://www.abukhleif.com/",
+    // "akf": "https://www.abukhleif.com/",
     "apps": "https://play.google.com/store/apps/dev?id=6213463466804826025",
     "cv": "https://drive.google.com/file/d/0B2215F1Cj2_0ZF9FVFlpNXhSUmM/view",
     "twitter": "https://twitter.com/mohnoor94",
@@ -10,33 +10,33 @@ const redirections = {
     "email": "mailto:moh.noor94@gmail.com",
     "youtube": "https://www.youtube.com/c/CodewithNoor/",
     "youtube-main": "https://www.youtube.com/c/MohammadNoorAbuKhleif",
-    "ig": "https://www.instagram.com/mohnoor94/",
+    "ig": "https://www.instagram.com/mohnoor1994/",
     "sof": "https://stackoverflow.com/users/2847543/mohammad-noor",
     "hr": "https://www.hackerrank.com/moh_noor94",
-    "interview": "https://present.noor.guru/tech-interview",
-    "tech-job": "https://present.noor.guru/tech-job/",
+    "interview": "https://present.bynoor.io/tech-interview",
+    "tech-job": "https://present.bynoor.io/tech-job/",
     
     
     // C++ Files
-    "cpp-fts": "https://1drv.ms/b/s!AnR5_MGIlB3e72UGY6djrr2v5uXy",
-    "cpp-ftq1": "https://1drv.ms/b/s!AnR5_MGIlB3e72ki6llgN-pEmQ5Z",
-    "cpp-ftq2": "https://www.abukhleif.com/first-exam-important-questions/",
-    "cpp-sds1": "https://1drv.ms/b/s!AnR5_MGIlB3e72Soidf48IJz93eA",
-    "cpp-sds2": "https://1drv.ms/b/s!AnR5_MGIlB3e72NVQyjQfq1ysj_8",
-    "cpp-sdq1": "https://1drv.ms/b/s!AnR5_MGIlB3e70lVF0ECLRIpd8tm",
-    "cpp-sdq2": "https://www.abukhleif.com/cpp-second-exam-write-function-past-papers/",
-    "cpp-sdq3": "https://www.abukhleif.com/pattern-programs/",
-    "cpp-fls": "https://1drv.ms/b/s!AnR5_MGIlB3e70iHhYr8aGXhtPuC",
-    "cpp-flq": "https://1drv.ms/b/s!AnR5_MGIlB3e70W4gP1sjOm4iea2",
-    "cpp-ex": "https://www.abukhleif.com/category/c/cppexercises/",
+    // "cpp-fts": "https://1drv.ms/b/s!AnR5_MGIlB3e72UGY6djrr2v5uXy",
+    // "cpp-ftq1": "https://1drv.ms/b/s!AnR5_MGIlB3e72ki6llgN-pEmQ5Z",
+    // "cpp-ftq2": "https://www.abukhleif.com/first-exam-important-questions/",
+    // "cpp-sds1": "https://1drv.ms/b/s!AnR5_MGIlB3e72Soidf48IJz93eA",
+    // "cpp-sds2": "https://1drv.ms/b/s!AnR5_MGIlB3e72NVQyjQfq1ysj_8",
+    // "cpp-sdq1": "https://1drv.ms/b/s!AnR5_MGIlB3e70lVF0ECLRIpd8tm",
+    // "cpp-sdq2": "https://www.abukhleif.com/cpp-second-exam-write-function-past-papers/",
+    // "cpp-sdq3": "https://www.abukhleif.com/pattern-programs/",
+    // "cpp-fls": "https://1drv.ms/b/s!AnR5_MGIlB3e70iHhYr8aGXhtPuC",
+    // "cpp-flq": "https://1drv.ms/b/s!AnR5_MGIlB3e70W4gP1sjOm4iea2",
+    // "cpp-ex": "https://www.abukhleif.com/category/c/cppexercises/",
 
     
     // C++ Quizzes
-    "cpp-qft1": "https://www.abukhleif.com/quiz1/",
-    "cpp-qft2": "https://www.abukhleif.com/quiz2/",
-    "cpp-qft4": "https://www.abukhleif.com/quiz4/",
-    "cpp-eft1": "https://www.abukhleif.com/first-exam1/",
-    "cpp-eft2": "https://www.abukhleif.com/first-exam2/",
+    // "cpp-qft1": "https://www.abukhleif.com/quiz1/",
+    // "cpp-qft2": "https://www.abukhleif.com/quiz2/",
+    // "cpp-qft4": "https://www.abukhleif.com/quiz4/",
+    // "cpp-eft1": "https://www.abukhleif.com/first-exam1/",
+    // "cpp-eft2": "https://www.abukhleif.com/first-exam2/",
 };
 
 const getLongUrl = (shortUrl) => redirections[shortUrl] || "https://www.bynoor.io/";
